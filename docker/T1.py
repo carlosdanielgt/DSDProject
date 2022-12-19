fb36d6662113754bb1064404d74f3158d9d1ce11
Shubham patel <59463910+009geniuS@users.noreply.github.com>
2022-12-18 20:53:13 -0500

    Update T1.py

--- docker/T1.py
+++ docker/T1.py
@@ -1,9 +1,34 @@
 import pandas as pd
+import requests
+import boto3
 
-dataset = './en-fr.csv'
-df = pd.read_csv(dataset,nrows = 50000,header=None)
+dynamodb = boto3.resource('dynamodb', endpoint_url='http://localhost:8000')
+table = dynamodb.Table('translations')
+
+def get_translation_en_to_fr(string):
+    response = table.get_item(
+        Key={
+            'en': string
+        }
+    )
+    if "Item" in response:
+        item = response['Item']
+        french = item['fr']
+        return french
+    else:
+        return ''
 
 def executeMpi(data):
-    op = df[df.iloc[:, 0] == data['sentence']]
-    finalResult = op.iloc[:,1].values.tolist()
+    finalResult = get_translation_en_to_fr(data['sentence'])
+    if finalResult=='':
+        
+        url = "https://deep-translator-api.azurewebsites.net/google/"
+
+        querystring = {"source": "english",
+        "target": "french",
+        "text": data['sentence'],
+        "proxies": []}
+        
+        response = requests.request("POST", url,  json=querystring)
+        finalResult = [str(response.json()['translation'])]
     return str(finalResult)
